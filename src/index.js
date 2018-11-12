b6acee513ef6f2f1410a51b45d11b324f795fce6
Sergey Makovetsky <sergey.makovetskiy@softesis.com>
2018-11-12 15:44:50 +0200

    Mpulse test

--- src/index.js
+++ src/index.js
@@ -2,16 +2,58 @@ import DevTools from 'mobx-react-devtools';
 import GlobalContainer from './components/GlobalContainer';
 import GlobalContainerModel from './models/GlobalContainerModel';
 import React from 'react';
+import { Router, Route, Link } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory'
+
 
 import { render } from 'react-dom';
 
 const store = new GlobalContainerModel();
 
+const history = createHistory({
+  basename: '/'
+})
+
+function hookHistoryBoomerang() {
+  if (window.BOOMR && BOOMR.version) {
+      if (BOOMR.plugins && BOOMR.plugins.History) {
+          BOOMR.plugins.History.hook(history, true);
+      }
+      return true;
+  }
+}
+
+if (!hookHistoryBoomerang()) {
+  if (document.addEventListener) {
+      document.addEventListener("onBoomerangLoaded", hookHistoryBoomerang);
+  } else if (document.attachEvent) {
+      document.attachEvent("onpropertychange", function(e) {
+          e = e || window.event;
+          if (e && e.propertyName === "onBoomerangLoaded") {
+              hookHistoryBoomerang();
+          }
+      });
+  }
+}
+
+const unlisten = history.listen((location, action) => {
+  // location is an object like window.location
+  console.log(action, location.pathname, location.state)
+})
+
+const Index = () => <div>Index</div>;
+const About = () => <div>About</div>;
+
 render(
-  <div>
-    <DevTools />
-    <GlobalContainer store={store} />
-  </div>,
+  <Router history={history}>
+    <div>
+      <DevTools />
+      <Link to="/">Home</Link>
+      <Link to="/about/">About</Link>
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
 
